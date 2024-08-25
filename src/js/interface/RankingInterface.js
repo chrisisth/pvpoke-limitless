f415de0ce611877cfd4026b1f11ec0dbeb412021
Chrisforsure <61108153+chrisisth@users.noreply.github.com>
2024-08-25 17:07:44 +0200

    .

--- src/js/interface/RankingInterface.js
+++ src/js/interface/RankingInterface.js
@@ -219,7 +219,7 @@ var InterfaceMaster = (function () {
 
 				csv = 'Pokemon,Score,Dex,Type 1,Type 2,Attack,Defense,Stamina,Stat Product,Level,CP,Fast Move,Charged Move 1,Charged Move 2,Charged Move 1 Count,Charged Move 2 Count,Buddy Distance,Charged Move Cost\n';
 
-					var customMetaSelector = new PokeMultiSelect($(".poke.multi").eq(0));
+				var customMetaSelector = new PokeMultiSelect($(".poke.multi").eq(0));
 				customMetaSelector.init(data.pokemon, battle);
 				//customMetaSelector.setContext("customrankings");
 
