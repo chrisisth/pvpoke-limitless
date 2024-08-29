5afb22e743589475f013d806d5a3cd849691cd82
Chrisforsure <61108153+chrisisth@users.noreply.github.com>
2024-08-29 20:15:17 +0200

    finished tampermonkey script

--- pvpokelmtlss.js
+++ pvpokelmtlss.js
@@ -7,180 +7,151 @@
 // @match        *://pvpoke.com/*
 // @grant        GM.xmlHttpRequest
 // @grant        unsafeWindow
+// @run-at       document-start
 // ==/UserScript==
 
 (function () {
   "use strict";
   var url = window.location.href;
 
-  //Enable save rankings to group
-  if (url.includes("/new-season/rankings") || url.includes("/rankings")) {
-    GM.xmlHttpRequest({
-      method: "GET",
-      url: "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/Pokebox.js",
-      onload: function (response) {
-        const script = document.createElement("script");
-        script.textContent = response.responseText;
-        document.head.appendChild(script);
-      },
-      onerror: function (error) {
-        console.error("Error loading script:", error);
-      },
-    });
+  function SaveRankingButton() {
+    var customMetaSelector = new PokeMultiSelect($(".poke.multi").eq(0));
+    var data = GameMaster.getInstance().data;
+    customMetaSelector.init(data.pokemon, new Battle());
+    customMetaSelector.setPokemonList(metaGroupExport);
 
-    GM.xmlHttpRequest({
-      method: "GET",
-      url: "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/PokeMultiSelect.js",
-      onload: function (response) {
-        const script = document.createElement("script");
-        script.textContent = response.responseText;
-        document.head.appendChild(script);
-      },
-      onerror: function (error) {
-        console.error("Error loading script:", error);
-      },
-    });
+    var cup = document.getElementsByClassName("format-select")[0];
+    cup = cup.options[cup.selectedIndex];
+    var cp = cup.value;
+    cup = cup.getAttribute("meta-group");
 
-    GM.xmlHttpRequest({
-      method: "GET",
-      url: "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/RankingInterface.js",
-      onload: function (response) {
-        const customScriptContent = response.responseText;
+    var category = document.getElementsByClassName("category-select")[0];
+    category = category.options[category.selectedIndex];
+    category = category.value;
 
-        // Replace RankingInterface.js
-        function replaceScript(oldScriptSrc, newScriptContent) {
-          const scripts = document.getElementsByTagName("script");
-          for (let i = 0; i < scripts.length; i++) {
-            if (scripts[i].src.includes(oldScriptSrc)) {
-              const newScript = document.createElement("script");
-              newScript.textContent = newScriptContent;
-              scripts[i].parentNode.replaceChild(newScript, scripts[i]);
-              break;
-            }
-          }
-        }
-        replaceScript("RankingInterface.js", customScriptContent);
-        console.log("Replaced");
-      },
-      onerror: function (error) {
-        console.error("Error loading script:", error);
-      },
-    });
-
-    function SaveRankingButton() {
-      var customMetaSelector = new PokeMultiSelect($(".poke.multi").eq(0));
-      var data = GameMaster.getInstance().data;
-      customMetaSelector.init(data.pokemon, new Battle());
-      customMetaSelector.setPokemonList(metaGroupExport);
-
-      var cup = document.getElementsByClassName("format-select")[0];
-      cup = cup.options[cup.selectedIndex];
-      var cp = cup.value;
-      cup = cup.getAttribute("meta-group");
-
-      var category = document.getElementsByClassName("category-select")[0];
-      category = category.options[category.selectedIndex];
-      category = category.value;
-
-      customMetaSelector.saveCustomList(cp + cup + category, false);
-    }
-
-    var button = document.createElement("a");
-    button.className = "button";
-    button.textContent = "Save Rankings";
-
-    button.addEventListener("click", function () {
-      SaveRankingButton();
-    });
-
-    var exportButton = document.querySelector("a.download-csv");
-    if (exportButton) {
-      exportButton.parentNode.insertBefore(button, exportButton.nextSibling);
-    }
+    customMetaSelector.saveCustomList(cp + cup + category, false);
   }
 
-  if (url.includes("/new-season/battle") || url.includes("/battle")) {
-
-    GM.xmlHttpRequest({
-      method: "GET",
-      url: "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/PokeMultiSelect.js",
-      onload: function (response) {
-        const customScriptContent = response.responseText;
-
-        function replaceScript(oldScriptSrc, newScriptContent) {
-          const scripts = unsafeWindow.document.getElementsByTagName("script");
-          for (let i = 0; i < scripts.length; i++) {
-            if (scripts[i].src.includes(oldScriptSrc)) {
-              const newScript = unsafeWindow.document.createElement("script");
-              newScript.textContent = newScriptContent;
-              scripts[i].parentNode.replaceChild(newScript, scripts[i]);
-              break;
+  function BlockScript(script) {
+    var observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        if (mutation.addedNodes.length) {
+          for (var i = 0; i < mutation.addedNodes.length; i++) {
+            var node = mutation.addedNodes[i];
+            if (node.tagName === "SCRIPT" && node.src.includes(script)) {
+              node.parentNode.removeChild(node);
             }
           }
         }
-        // Replace RankingInterface.js
-        replaceScript("PokeMultiSelect.js", customScriptContent);
-        console.log("Replaced");
+      });
+    });
+
+    observer.observe(document, { childList: true, subtree: true });
+  }
+
+  function LoadScript(scripturl) {
+    GM.xmlHttpRequest({
+      method: "GET",
+      url: scripturl,
+      onload: function (response) {
+        const script = document.createElement("script");
+        script.textContent = response.responseText;
+        document.head.appendChild(script);
       },
       onerror: function (error) {
         console.error("Error loading script:", error);
       },
     });
+  }
+
+  if (url.includes("/new-season/rankings") || url.includes("/rankings")) {
+    BlockScript("Pokebox.js");
+    BlockScript("PokeMultiSelect.js");
+    BlockScript("RankingInterface.js");
+
+    LoadScript(
+      "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/Pokebox.js"
+    );
+    LoadScript(
+      "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/PokeMultiSelect.js"
+    );
+    LoadScript(
+      "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/RankingInterface.js"
+    );
+
+    window.onload = function () {
+      var button = document.createElement("a");
+      button.className = "button";
+      button.textContent = "Save Rankings";
+
+      button.addEventListener("click", function () {
+        SaveRankingButton();
+      });
+
+      var exportButton = document.querySelector("a.download-csv");
+      if (exportButton) {
+        exportButton.parentNode.insertBefore(button, exportButton.nextSibling);
+      }
+      Main();
+    };
+  }
+  if (url.includes("/new-season/battle") || url.includes("/battle")) {
+    BlockScript("PokeMultiSelect.js");
+    BlockScript("RankingInterface.js");
+
+    LoadScript(
+      "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/Pokebox.js"
+    );
+    LoadScript(
+      "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/PokeMultiSelect.js"
+    );
+    window.onload = function () {
+      Main();
+      $(".poke-max-count").text("1000");
+    };
   }
 
   if (
     url.includes("/new-season/team-builder") ||
     url.includes("/team-builder")
   ) {
+    BlockScript("PokeMultiSelect.js");
+    BlockScript("RankingInterface.js");
 
-    GM.xmlHttpRequest({
-      method: "GET",
-      url: "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/PokeMultiSelect.js",
-      onload: function (response) {
-        const customScriptContent = response.responseText;
-
-        function replaceScript(oldScriptSrc, newScriptContent) {
-          const scripts = unsafeWindow.document.getElementsByTagName("script");
-          for (let i = 0; i < scripts.length; i++) {
-            if (scripts[i].src.includes(oldScriptSrc)) {
-              const newScript = unsafeWindow.document.createElement("script");
-              newScript.textContent = newScriptContent;
-              scripts[i].parentNode.replaceChild(newScript, scripts[i]);
-              break;
-            }
-          }
-        }
-        // Replace RankingInterface.js
-        replaceScript("PokeMultiSelect.js", customScriptContent);
-        console.log("Replaced");
-      },
-      onerror: function (error) {
-        console.error("Error loading script:", error);
-      },
-    });
-
-    // Team Builder - team size = 3
-    var teamOption = document.querySelector(".team-option .team-size-select");
-    if (teamOption) {
-      var newOption = document.createElement("option");
-      newOption.value = "3";
-      newOption.textContent = "3";
-      teamOption.appendChild(newOption);
-    }
-
-    // Team Builder - scorecard increase
-    var scorecardSelect = document.querySelector(
-      ".team-option .scorecard-length-select"
+    LoadScript(
+      "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/Pokebox.js"
     );
-    if (scorecardSelect) {
-      var values = [100, 200, 300, 400, 500];
-      values.forEach(function (value) {
-        var option = document.createElement("option");
-        option.value = value;
-        option.textContent = value;
-        scorecardSelect.appendChild(option);
-      });
-    }
-  }
+    LoadScript(
+      "https://raw.githubusercontent.com/chrisisth/pvpoke-limitless/master/src/js/interface/PokeMultiSelect.js"
+    );
 
+    window.onload = function () {
+      // Team Builder - team size = 3
+      var teamOption = document.querySelector(".team-option .team-size-select");
+      if (teamOption) {
+        var newOption = document.createElement("option");
+        newOption.value = "3";
+        newOption.textContent = "3";
+        teamOption.appendChild(newOption);
+      }
+
+      // Team Builder - scorecard increase
+      var scorecardSelect = document.querySelector(
+        ".team-option .scorecard-length-select"
+      );
+      if (scorecardSelect) {
+        var values = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+        values.forEach(function (value) {
+          var option = document.createElement("option");
+          option.value = value;
+          option.textContent = value;
+          scorecardSelect.appendChild(option);
+        });
+      }
+      Main();
+      $(".custom-alternatives .poke.multi .poke-max-count").text("1000");
+      $(".exclude-alternatives .poke.multi .poke-max-count").text("1000");
+      $(".exclude-threats .poke.multi .poke-max-count").text("1000");
+    };
+  }
 })();
